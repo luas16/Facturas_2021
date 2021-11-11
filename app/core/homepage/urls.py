4b56737659f713d5de11babf6bae7528405aca01
luas <coy.saul@gmail.com>
2021-11-11 05:01:07 -0600

    app_ControldeGastos

---
+++ app/core/homepage/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from core.homepage.views import *
+app_name = 'home'
+
+urlpatterns = [
+    path('index', IndexView.as_view(), name='index'),
+    # path('nosotros/', NosotrosView.as_view(), name='nosotros'),
+    # path('anuncios/', AnunciosView.as_view(), name='anuncios'),
+    # path('proceso/', ProcesosView.as_view(), name='procesos'),
+    # path('productos/', ProductosView.as_view(), name='productos'),
+
+]
